4de29fba2e29bf23e93534aed98066aafc91e329
Petr Joachim <petr@joachim.cz>
2015-04-22 20:52:31 +0200

    zapnuti registraci a zapnuti hlasovani

--- debug.py
+++ debug.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         'TESTING': True,
         'SECRET_KEY': 'jednadvehonzajde',
         'YEAR': "2015",
-        'STAGES': ['REGISTER_TALKS']
+        'STAGES': ['REGISTER_TALKS', 'VOTING', 'USERS']
     }
 
     config.update(os.environ)
--- heroku.py
+++ heroku.py
@@ -3,7 +3,7 @@ from barcamp import create_app
 
 config = {
     'YEAR': "2015",
-    'STAGES': ["REGISTER_TALKS"],
+    'STAGES': ['REGISTER_TALKS', 'VOTING', 'USERS'],
     'MAIL_SERVER': 'smtp.mandrillapp.com',
     'MAIL_PORT': 587,
     'MAIL_USERNAME': os.environ.get('MANDRILL_USERNAME', ''),
