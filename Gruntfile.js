deb14bfe4c5a636cec66bfcd7331d87d8b6ff9d4
Petr Joachim <petr@joachim.cz>
2016-03-05 15:38:09 +0100

    feat: add grunt less compiler

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,29 @@
 module.exports = function(grunt) {
-	grunt.registerTask('default', []);
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          // compress: true,
+          // yuicompress: true,
+          // optimization: 2
+        },
+        files: {
+          "static/css/main.css": "static/less/main.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['static/less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less']);
 };
