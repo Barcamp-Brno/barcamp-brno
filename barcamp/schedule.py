a3656d26799c85cb002570c258d7a88b589e9030
Petr Joachim <petr@joachim.cz>
2016-03-13 13:38:23 +0100

    feat: add schedule data

---
+++ barcamp/schedule.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+"""
+    Schedule settings
+"""
+
+from datetime import datetime
+
+
+schedule = {
+    'YEAR': '2016',
+    'YEAR_ARCHIVE': ['2013', '2014', '2015'],
+    'YEAR_ENABLED': ['2016'],
+    'YEAR_SCHEDULE': {
+        '2016': {
+            'DATE': datetime(2016, 6, 4),
+            'STAGES': {
+                'PREVIEW': {
+                    'from': datetime(1970, 1, 1),
+                    'to': datetime(2016, 3, 12),
+                },
+                'CALL-FOR-PAPERS': {
+                    'from': datetime(2016, 3, 13),
+                    'to': datetime(2016, 5, 21),
+                },
+            },
+        },
+        '2017': {
+            'DATE': datetime(2017, 6, 3),
+            'STAGES': {
+                'PREVIEW': {
+                    'from': datetime(1970, 1, 1),
+                    'to': datetime(2017, 3, 11),
+                },
+            },
+        },
+    },
+}
