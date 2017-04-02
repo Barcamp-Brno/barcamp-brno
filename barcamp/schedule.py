0f93aab54ec91c89365a55a5851b6e4874b19681
Petr Joachim <petr@joachim.cz>
2017-04-02 20:18:16 +0200

    Schedule new barcamp

--- barcamp/schedule.py
+++ barcamp/schedule.py
@@ -15,26 +15,6 @@ schedule = {
         # '2016': {
         #     'DATE': datetime(2016, 6, 4),
         #     'STAGES': {
-        #         'PREVIEW': {
-        #             'from': datetime(1970, 1, 1),
-        #             'to': datetime(2016, 3, 12),
-        #         },
-        #         'CALL-FOR-PAPERS': {
-        #             'from': datetime(2016, 3, 13),
-        #             'to': datetime(2016, 5, 21),
-        #         },
-        #         'CALL-FOR-WORKSHOPS': {
-        #             'from': datetime(2016, 4, 10),
-        #             'to': datetime(2016, 4, 24),
-        #         },
-        #         'VOTING': {
-        #             'from': datetime(2016, 5, 1),
-        #             'to': datetime(2016, 5, 21),
-        #         },
-        #         'USERS': {
-        #             'from': datetime(2016, 5, 1),
-        #             'to': datetime(2016, 6, 4),
-        #         },
         #         'T-SHIRTS': {
         #             'from': datetime(2016, 4, 22),
         #             'to': datetime(2016, 5, 13),
@@ -60,7 +40,24 @@ schedule = {
                     'from': datetime(1970, 1, 1),
                     'to': datetime(2017, 3, 11),
                 },
+                'CALL-FOR-PAPERS': {
+                    'from': datetime(2017, 4, 1),
+                    'to': datetime(2017, 5, 21),
+                },
+                'CALL-FOR-WORKSHOPS': {
+                    'from': datetime(2017, 4, 1),
+                    'to': datetime(2017, 4, 23),
+                },
+                'VOTING': {
+                    'from': datetime(2017, 5, 1),
+                    'to': datetime(2017, 5, 21),
+                },
+                'USERS': {
+                    'from': datetime(2017, 5, 1),
+                    'to': datetime(2017, 6, 3),
+                },
             },
+            
         },
     },
 }
