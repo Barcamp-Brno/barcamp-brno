044c79f863f72e7b19916f52aa5ec21a28995a3c
Petr Joachim <petr@joachim.cz>
2019-09-03 12:43:34 +0200

    Add google login

--- barcamp/login_oauth.py
+++ barcamp/login_oauth.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from flask_dance.contrib.facebook import facebook, make_facebook_blueprint
+from flask_dance.contrib.google import google, make_google_blueprint
 from flask_dance.consumer import oauth_authorized
 from flask import session, url_for, request, flash, redirect
 from .login_misc import resolve_user_by_email, create_account, authorized_redirect
@@ -30,7 +31,7 @@ def connect_facebook():
     session['user_hash'] = user_hash  # prihlaseni hotovo
 
     if new_account:
-        flash(u'Váš facebook účet je spárován', 'success')
+        flash(u'Váš Facebook účet je spárován', 'success')
 
     session['next'] = None
     return authorized_redirect(next or url_for('login_settings'))
@@ -44,3 +45,38 @@ facebook_blueprint = make_facebook_blueprint(
     redirect_to='connect_facebook'
 )
 app.register_blueprint(facebook_blueprint, url_prefix='/login/oauth')
+
+@app.route('/connect/google')
+def connect_google():
+    next = session.get('next', None)
+    resp = google.get("/oauth2/v1/userinfo")
+    data = resp.json()
+
+    if not data.get('email'):
+        flash(u'Nepodařilo se získat e-mail z Google, zkuste to znovu.', 'danger')
+        return redirect(url_for('login'))
+
+    user_hash = resolve_user_by_email(data['email'])
+    new_account = False
+    if not user_hash:
+        # pokud ucet neexistuje, vytvorime (a vyplnime) ho z FB
+        user_hash = create_account(data['email'], None, data={
+            'verified': True,
+            'name': data.get('name', ''),
+        })
+
+        new_account = True
+    session['user_hash'] = user_hash  # prihlaseni hotovo
+    if new_account:
+        flash(u'Váš Google účet je spárován', 'success')
+
+    session['next'] = None
+    return authorized_redirect(next or url_for('login_settings'))
+
+google_blueprint = make_google_blueprint(
+    client_id = app.config['GOOGLE_ID'],
+    client_secret = app.config['GOOGLE_SECRET'],
+    scope="openid https://www.googleapis.com/auth/userinfo.email",
+    redirect_to="connect_google"
+)
+app.register_blueprint(google_blueprint, url_prefix='/login/oauth')
