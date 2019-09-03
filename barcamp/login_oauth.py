52f0427f0675c026bd16d648167d2ac1842fe010
Petr Joachim <petr@joachim.cz>
2019-09-03 13:00:16 +0200

    Add linkedin login - hidden so far

--- barcamp/login_oauth.py
+++ barcamp/login_oauth.py
@@ -2,6 +2,7 @@
 
 from flask_dance.contrib.facebook import facebook, make_facebook_blueprint
 from flask_dance.contrib.google import google, make_google_blueprint
+from flask_dance.contrib.linkedin import linkedin, make_linkedin_blueprint
 from flask_dance.consumer import oauth_authorized
 from flask import session, url_for, request, flash, redirect
 from .login_misc import resolve_user_by_email, create_account, authorized_redirect
@@ -80,3 +81,41 @@ google_blueprint = make_google_blueprint(
     redirect_to="connect_google"
 )
 app.register_blueprint(google_blueprint, url_prefix='/login/oauth')
+
+
+@app.route('/connect/linkedin')
+def connect_linkedin():
+    next = session.get('next', None)
+    resp = linkedin.get("/v2/me")
+    data = resp.json()
+
+    1/0
+
+    if not data.get('email'):
+        flash(u'Nepodařilo se získat e-mail z Linkedin, zkuste to znovu.', 'danger')
+        return redirect(url_for('login'))
+
+    user_hash = resolve_user_by_email(data['email'])
+    new_account = False
+    if not user_hash:
+        # pokud ucet neexistuje, vytvorime (a vyplnime) ho z FB
+        user_hash = create_account(data['email'], None, data={
+            'verified': True,
+            'name': data.get('name', ''),
+        })
+
+        new_account = True
+    session['user_hash'] = user_hash  # prihlaseni hotovo
+    if new_account:
+        flash(u'Váš Linkedin účet je spárován', 'success')
+
+    session['next'] = None
+    return authorized_redirect(next or url_for('login_settings'))
+
+linkedin_blueprint = make_linkedin_blueprint(
+    client_id = app.config['LINKEDIN_ID'],
+    client_secret = app.config['LINKEDIN_SECRET'],
+    scope="email",
+    redirect_to="connect_linkedin"
+)
+app.register_blueprint(linkedin_blueprint, url_prefix="/login/oauth")
